0331b2f774f928002af30ef9c61c0e349f720b8c
ansk1d <anasalkhaled@aol.com>
2022-07-31 08:05:57 +0300

    frut

---
+++ src/test/java/com/anas/fruityloops/FruityLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anas.fruityloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityLoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
